9aa77b092488bcaeb96bc4e5f1f6cd611fe2cbaa
Abhishek Chhawari <abhishekch360@gmail.com>
2023-04-06 14:46:05 +0530

    array dest and spread

--- destructing.js
+++ destructing.js
@@ -3,3 +3,18 @@ const arrayB = ['golem','irfan','nikhil']
 
 const newArray = [...arrayA, ...arrayB]
 console.log(newArray)
+
+//
+
+const alphabets = ['A','B','C','D','E']
+
+const[a,b] = alphabets
+console.log(`${a} and ${b}`)
+
+//skips the between
+const[c,,d] = alphabets
+console.log(`${c} and ${d}`)
+
+//spread operator
+const [x,y,...rest] = alphabets
+console.log(rest)
