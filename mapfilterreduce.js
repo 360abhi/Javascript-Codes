9f511a470c15218cfd16e892e75b46457c42079d
Abhishek Chhawari <abhishekch360@gmail.com>
2023-04-07 17:28:09 +0530

    map filter reduce

---
+++ mapfilterreduce.js
@@ -0,0 +1,44 @@
+//map
+const arr = [-3,-5,-2,-3]
+//square
+console.log(arr.map((n)=>n*n));
+// double
+console.log(arr.map((n)=>2*n));
+//binary
+console.log(arr.map((n)=>n.toString(2)));
+
+// filter
+
+const array =[4,7,6,5,9];
+//filter odd values
+console.log(array.filter((n)=>n%2==1));
+//filter even values
+console.log(array.filter((n)=>n%2==0));
+
+//reduce
+
+//sum or max
+function sum(arr){
+    let sum=0;
+    for(let ele of arr){
+        sum += ele;
+    }
+    return sum;
+}
+
+console.log(sum(arr));
+
+//using reduce
+//acc is like sum and curr is the elemts iterated through
+//0 is the intial val of acc
+console.log(arr.reduce((acc,curr)=>{
+    acc += curr;
+    return acc;
+},0));
+
+//max
+
+console.log(arr.reduce((acc,curr)=>{
+    acc = Math.max(acc,curr);
+    return acc;
+},-Infinity));
