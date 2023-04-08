9113ef6621acab0c9efea005db3b1d72237ac619
Abhishek Chhawari <abhishekch360@gmail.com>
2023-04-08 18:05:16 +0530

    callback funtions

---
+++ callbacks.js
@@ -0,0 +1,22 @@
+// a callback is a function passed as an argument to another function
+
+function greet(name, callback){
+    console.log("Hello "+name);
+    callback();
+}
+
+function callme(){
+    console.log("I am a callback function");
+}
+
+greet('Abhishek',callme);
+
+const calculate = (a,b,operation) => {
+    return operation(a,b);
+}
+
+const res =calculate(3,4,(m,n)=>{
+    return m+n;
+})
+
+console.log(res)
